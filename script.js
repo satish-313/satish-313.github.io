abc1f813054f8c46b0254bfad95858b3557a4199
satish-313 <pradhansatish53@gmail.com>
2023-09-20 20:18:18 +0530

    completed about page

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
-const myIntro = [
-  "I am Satish Kumar",
-  "Full stack web developer",
-  "Expertise in TypeScript, JavaScript",
-];
-// let hand = 👋;
+const myIntro = ["I am Satish Kumar", "NodeJs developer", "Frontend developer"];
 let index = 0;
 let currentlineIndex = 0;
 let currentLine = [];
 let isAdding;
 let isRemoving;
-
 const introEle = document.getElementById("intro");
 
+let allSection = document.querySelectorAll("section");
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            entry.target.classList.toggle(
+                "scroll-animation",
+                entry.isIntersecting
+            );
+        });
+    },
+    {
+        threshold: 0.3,
+    }
+);
+
+allSection.forEach((s) => observer.observe(s));
+
 function showIntro() {
-  while (index < myIntro.length) {
-    if (currentlineIndex === 0 && isRemoving) {
-      isRemoving = false;
-      index = (index + 1) % myIntro.length;
-      continue;
-    }
-    if (currentlineIndex >= myIntro[index].length) {
-      isAdding = false;
-      isRemoving = true;
-    }
-    if (currentlineIndex < myIntro[index].length) isAdding = true;
+    while (index < myIntro.length) {
+        if (currentlineIndex === 0 && isRemoving) {
+            isRemoving = false;
+            index = (index + 1) % myIntro.length;
+            continue;
+        }
+        if (currentlineIndex >= myIntro[index].length) {
+            isAdding = false;
+            isRemoving = true;
+        }
+        if (currentlineIndex < myIntro[index].length) isAdding = true;
 
-    if (isAdding && !isRemoving) {
-      let char = myIntro[index][currentlineIndex++];
-      currentLine.push(char);
-      let temp = currentLine.join("");
+        if (isAdding && !isRemoving) {
+            let char = myIntro[index][currentlineIndex++];
+            currentLine.push(char);
+            let temp = currentLine.join("");
+            introEle.innerHTML = temp;
+        }
 
-      introEle.innerHTML = temp;
+        if (isRemoving) {
+            currentLine.pop();
+            if (currentLine.length === 0)
+                introEle.innerHTML = myIntro[index][0];
+            else introEle.innerHTML = currentLine.join("");
+            currentlineIndex -= 1;
+        }
+        break;
     }
-
-    if (isRemoving) {
-      currentLine.pop();
-      introEle.innerHTML = currentLine.join("");
-      currentlineIndex -= 1;
-    }
-    break;
-  }
 }
 
-// setInterval(() => {
-//   showIntro();
-// }, 500);
+setInterval(() => {
+    showIntro();
+}, 400);
